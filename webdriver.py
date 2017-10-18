5c9a0143be96b81f1d7d9a66d1af6ff0a7eafa80
Hydur <cflightfoot@gmail.com>
2017-10-18 11:05:14 -0400

    Fixed issue for when there are no further search results to look at when finding URLs

--- webdriver.py
+++ webdriver.py
@@ -2,7 +2,7 @@
 # https://stackoverflow.com/questions/13287490/is-there-a-way-to-use-phantomjs-in-python
 
 # TODO: Look at pp. 264 - 275 for further information regarding Flask-based web apps
-# TODO: 
+# TODO: Run scraped texts against ML algorithm to generate a number
 from flask import Flask, render_template, request
 from wtforms import Form, TextAreaField, validators
 from selenium import webdriver
@@ -11,6 +11,21 @@ from string import Template
 import time
 import requests
 from bs4 import BeautifulSoup
+
+conservativeURL = ' '
+liberalURL = ' '
+cLinkName = ' '
+lLinkName = ' '
+
+
+# TODO: Run text through algorithm and determine if url should fill either conservative
+#       or liberal URL spots on webpage (will need to determine threshold values)
+def checkURL(url, text, linkName):
+    global conservativeURL
+    global liberalURL
+    global cLinkName
+    global lLinkName
+    pass
 
 app = Flask(__name__)
 
@@ -24,6 +39,12 @@ def index():
 
 @app.route('/', methods=['POST'])
 def results():
+    global conservativeURL
+    global liberalURL
+    global cLinkName
+    global lLinkName
+    errMessage = ' '
+
     form = SearchForm(request.form)
     if request.method == 'POST' and form.validate():
         inputString = request.form['inputString']
@@ -39,24 +60,29 @@ def results():
             # scrape first liberal and first conservative websites that are in the top 10 news websites
             # (top 10 according to http://blog.feedspot.com/usa_news_websites/ 's metrics)
             urls = driver.find_elements_by_css_selector('h3.r a')
-            conservativeURL = ' '
-            liberalURL = ' '
 
             # Continue mining until conservative- and liberalURL are found
             while conservativeURL == ' ' or liberalURL == ' ':
                 urls = driver.find_elements_by_css_selector('h3.r a')
                 for url in urls:
-                    # TODO: scrape according to each website's layout and then run ML algorithm
                     if conservativeURL != ' ' and liberalURL != ' ':
                         break
+
+                    # Need to remove end of links that make some webpages impossible
+                    # to link to via my webpage
                     stoppingPoint = url.get_attribute('href').index('&')
                     url = url.get_attribute('href')[29 : stoppingPoint]
                     print(url)
+
+                    # The queried search page is a url and needs to be skipped
                     if '?q=' in url:
                         continue
+
                     if "cnn.com" in url: # 1
-                        cLinkName = "CNN Article"
-                        conservativeURL = url
+                        #cLinkName = "CNN Article"
+                        #conservativeURL = url
+                        linkName = "CNN Article"
+                        
                         lookAtPage = requests.get(url)
                         soup = BeautifulSoup(lookAtPage.text, "html.parser")
                         paragraphs = soup.find_all('div', {"class":"zn-body__paragraph"})
@@ -64,46 +90,102 @@ def results():
                         for paragraph in paragraphs:
                             text = text + paragraph.text
                         #print(text)
+                        # TODO: For all links, check to see if they fit into either
+                        #       the conservative or liberal URL spots
+                        checkURL(url, text, linkName)
+
                     if "nytimes.com" in url: # 2
-                        lLinkName = "NY Times Article"
-                        liberalURL = url
+                        #lLinkName = "NY Times Article"
+                        #liberalURL = url
+                        linkName = "NY Times Article"
                         lookAtPage = requests.get(url)
                         soup = BeautifulSoup(lookAtPage.text, "html.parser")
                         paragraphs = soup.find_all('p', {"class":"story-body-text story-content"})
                         text = ''
                         for paragraph in paragraphs:
                             text = text + paragraph.text
+                        checkURL(url, text, linkName)
                         #print(text)
+
                     if "huffingtonpost.com" in url: # 3
-                        pass
+                        lLinkName = "Huffington Post"
+                        liberalURL = url
+                        #linkName = "Huffington Post"
+                        lookAtPage = requests.get(url)
+                        soup = BeautifulSoup(lookAtPage.text, "html.parser")
+                        #paragraphs = soup.find_all('p')
+                        text = ''
+                        #for paragraph in paragraphs:
+                            #text = text + paragraph.text
+                        print(text)
+                        checkURL(url, text, linkName)
+
                     if "foxnews.com" in url: # 4
-                        pass
+                        cLinkName = "Fox News"
+                        conservativeURL = url
+                        #linkName = "Fox News"
+                        lookAtPage = requests.get(url)
+                        #soup = BeautifulSoup(lookAtPage.text, "html.parser")
+                        checkURL(url, text, linkName)
+
                     if "usatoday.com" in url: # 5
                         pass
+                        linkName = "USA Today"
+                        lookAtPage = requests.get(url)
+                        soup = BeautifulSoup(lookAtPage.text, "html.parser")
+                        checkURL(url, text, linkName)
+
                     if "reuters.com" in url: # 6
                         pass
+                        linkName = "Reuters"
+                        lookAtPage = requests.get(url)
+                        soup = BeautifulSoup(lookAtPage.text, "html.parser")
+                        checkURL(url, text, linkName)
+
                     if "politico.com" in url: # 7
                         pass
+                        linkName = "Politico"
+                        lookAtPage = requests.get(url)
+                        soup = BeautifulSoup(lookAtPage.text, "html.parser")
+                        checkURL(url, text, linkName)
+
                     if "yahoo.com/news" in url: # 8
                         pass
+                        linkName = "Yahoo! News"
+                        lookAtPage = requests.get(url)
+                        soup = BeautifulSoup(lookAtPage.text, "html.parser")
+                        checkURL(url, text, linkName)
+
                     if "npr.org" in url: # 9
                         pass
+                        linkName = "NPR"
+                        checkURL(url, text, linkName)
+
                     if "latimes.com" in url: # 10
                         pass
+                        linkName = "LA Times"
+                        checkURL(url, text, linkName)
+
                     if "washingtonpost.com" in url: # requested
                         pass
+                        linkName = "Washington Post"
+                        checkURL(url, text, linkName)
 
                 if conservativeURL != ' ' and liberalURL != ' ':
                     break
-
+                driver.save_screenshot('screen.png') 
                 # Go to the next page to continue the process
-                nextPage = driver.find_element_by_link_text("Next").click()
+                try:
+                    nextPage = driver.find_element_by_link_text("Next").click()
+                except:
+                    errMessage = "Could not find enough sources on topic."
+                    break                    
             
             driver.save_screenshot('screen.png') # save a screenshot to disk to see what we're looking at
         driver.quit()
 
     return render_template('Website.html', cLinkName=cLinkName, lLinkName=lLinkName, \
-                            conservativeURL=conservativeURL, liberalURL=liberalURL)
+                            conservativeURL=conservativeURL, liberalURL=liberalURL, errMessage=errMessage)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
